a61d52fe3bed419f99d98413e7ecce84583fa660
Emine İNAN <56589369+emineinan@users.noreply.github.com>
2022-05-29 11:51:45 +0300

    Move BarChart codes from HomeFragment

--- app/src/main/java/com/yonder/weightly/ui/home/HomeFragment.kt
+++ app/src/main/java/com/yonder/weightly/ui/home/HomeFragment.kt
@@ -1,6 +1,5 @@
 package com.yonder.weightly.ui.home
 
-import android.graphics.Color
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
@@ -8,23 +7,13 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
-import com.github.mikephil.charting.components.Description
-import com.github.mikephil.charting.components.XAxis
-import com.github.mikephil.charting.data.BarData
-import com.github.mikephil.charting.data.BarDataSet
-import com.github.mikephil.charting.data.BarEntry
-import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
 import com.yonder.statelayout.State
 import com.yonder.weightly.R
 import com.yonder.weightly.databinding.FragmentHomeBinding
 import com.yonder.weightly.domain.uimodel.WeightUIModel
 import com.yonder.weightly.ui.home.adapter.WeightHistoryAdapter
 import com.yonder.weightly.ui.home.adapter.WeightItemDecorator
-import com.yonder.weightly.ui.home.chart.WeightMarkerView
-import com.yonder.weightly.ui.home.chart.WeightValueFormatter
-import com.yonder.weightly.ui.home.chart.XAxisValueDateFormatter
-import com.yonder.weightly.utils.extensions.EMPTY
-import com.yonder.weightly.utils.extensions.orZero
+import com.yonder.weightly.utils.chart.BarChartUtils
 import com.yonder.weightly.utils.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
@@ -67,13 +56,18 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         } else {
             binding.stateLayout.setState(State.CONTENT)
             adapterWeightHistory.submitList(uiState.reversedHistories)
-            setChartData(histories = uiState.histories, barEntries = uiState.barEntries)
+            BarChartUtils.setChartData(
+                barChart = binding.barChart,
+                histories = uiState.histories,
+                barEntries = uiState.barEntries,
+                context = requireContext()
+            )
         }
     }
 
     private fun initViews() {
         initWeightRecyclerview()
-        initBarChart()
+        BarChartUtils.initBarChart(binding.barChart)
     }
 
     private fun initWeightRecyclerview() = with(binding.rvWeightHistory) {
@@ -85,22 +79,6 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
                 DividerItemDecoration.VERTICAL
             )
         )
-    }
-
-    private fun initBarChart() = with(binding.barChart) {
-        legend.isEnabled = false
-        axisLeft.axisMinimum = 0.0f
-        xAxis.position = XAxis.XAxisPosition.BOTTOM
-        // Disable grid lines
-        xAxis.setDrawGridLines(false)
-        axisLeft.setDrawGridLines(false)
-        axisRight.setDrawGridLines(false)
-        //Disable zoom
-        isDoubleTapToZoomEnabled = false
-        setPinchZoom(false)
-        description = Description().apply {
-            text = String.EMPTY
-        }
     }
 
     private fun onClickWeight(weight: WeightUIModel) {
@@ -120,25 +98,4 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
             else -> super.onOptionsItemSelected(item)
         }
     }
-
-    private fun setChartData(histories: List<WeightUIModel?>, barEntries: List<BarEntry>) {
-        val set1 = BarDataSet(barEntries, String.EMPTY)
-        set1.valueFormatter = WeightValueFormatter(histories)
-        set1.valueTextSize = 9f
-        val xAxis = binding.barChart.xAxis
-        xAxis.labelCount = histories.size
-        xAxis.valueFormatter = XAxisValueDateFormatter(histories)
-        set1.color = Color.BLUE
-        val dataSets: java.util.ArrayList<IBarDataSet> = ArrayList()
-        dataSets.add(set1)
-        val data = BarData(dataSets)
-        binding.barChart.data = data
-        //Set marker view
-        val markerView = WeightMarkerView(requireContext(), histories)
-        markerView.chartView = binding.barChart
-        binding.barChart.marker = markerView
-
-        binding.barChart.invalidate()
-    }
-
 }
