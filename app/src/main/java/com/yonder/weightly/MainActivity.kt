8f059653d7548fe20ca56cd18c84df475d12a14b
Yusuf Önder <yusufonderder@gmail.com>
2022-06-12 10:57:31 +0300

    Remove bottom navigation bar

--- app/src/main/java/com/yonder/weightly/MainActivity.kt
+++ app/src/main/java/com/yonder/weightly/MainActivity.kt
@@ -1,18 +1,12 @@
 package com.yonder.weightly
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isGone
-import androidx.navigation.NavController
-import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.yonder.weightly.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -21,12 +15,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -38,13 +28,6 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_on_boarding
             )
         )
-        val noBottomNavigationIds = listOf(R.id.navigation_splash, R.id.navigation_on_boarding)
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            binding.navView.isGone = noBottomNavigationIds.contains(destination.id)
-        }
-
-
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
     }
 }
