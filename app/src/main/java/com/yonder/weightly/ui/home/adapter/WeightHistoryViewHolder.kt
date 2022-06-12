7d3f05a4ae3bab162fc01032b677f062ba074ecf
Yusuf Önder <yusufonderder@gmail.com>
2022-06-12 10:25:58 +0300

    Add app icon

--- app/src/main/java/com/yonder/weightly/ui/home/adapter/WeightHistoryViewHolder.kt
+++ app/src/main/java/com/yonder/weightly/ui/home/adapter/WeightHistoryViewHolder.kt
@@ -22,7 +22,6 @@ class WeightHistoryViewHolder(
         tvNote.text = uiModel.note
         tvEmoji.text = uiModel.emoji
         tvNote.isGone = uiModel.note.isBlank()
-        tvEmoji.isGone = uiModel.note.isBlank()
         tvDate.text = uiModel.date.toFormat(DATE_FORMAT)
         tvWeight.text = itemView.context.getString(R.string.kg_format, uiModel.value)
         itemView.setOnClickListener {
