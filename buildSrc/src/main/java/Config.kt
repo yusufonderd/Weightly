8cb4839a3a6a6f188b5906a4e012f43b6dde9b90
Yusuf Önder <yusufonderder@gmail.com>
2022-06-19 15:59:48 +0300

    Add kotlin dsl

---
+++ buildSrc/src/main/java/Config.kt
@@ -0,0 +1,10 @@
+
+object Config {
+    const val compileSdk = 32
+    const val minSdk = 21
+    const val targetSdk = 32
+    const val versionCode = 6
+    const val versionName = "1.0.5"
+    const val buildToolsVersion = "29.0.3"
+    const val applicationId = "com.yonder.weightly"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,17 @@
+object Versions {
+    const val stateLayout = "0.1.1"
+
+    const val coreKtx = "1.8.0"
+    const val appCompat = "1.4.2"
+    const val material = "1.6.1"
+    const val hilt = "2.42"
+    const val timber = "5.0.1"
+    const val room = "2.4.2"
+    const val boom = "30.1.0"
+    const val constraint = "2.1.4"
+    const val viewModel = "2.4.1"
+    const val navigation = "2.4.2"
+    const val preference = "1.2.0"
+    const val googleServices = "4.3.10"
+
+}
